442da7c434cf334f52e3d1c156eb7f92cb988cb3
Eduardo Braga <chocolate.blindado123@gmail.com>
2022-10-17 20:25:34 -0300

    Refactor: Separei o form do component home

---
+++ src/components/Form/index.jsx
@@ -0,0 +1,51 @@
+import React, {useContext, useEffect, useState} from "react"
+import { EntriesContext } from "../../provider/EntriesProvider"
+
+import { Container, InputRadio } from "./styles"
+
+export function Form({setVisibleModal}) {
+  const { AddEntrie, IdFromEntrie } = useContext(EntriesContext)
+
+  const [entrieType, setEntrieType] = useState(null)
+  const [entrieDescription, setEntrieDescription] = useState("")
+  const [entrieValue, setEntrieValue] = useState("")
+
+  const inputsRadios = document.querySelectorAll("input[name='entrieType']")
+
+  useEffect(() => {
+    const inputChecked = document.querySelector("input[name='entrieType']:checked").value
+    inputChecked === "entrada" ? setEntrieType(true) : setEntrieType(false)
+  }, [inputsRadios])
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    if (entrieDescription !== '' && entrieValue !== 0) {
+      setEntrieDescription('')
+      setEntrieValue('')
+      AddEntrie(entrieDescription, entrieValue, entrieType)
+      IdFromEntrie()
+    } else {
+      setVisibleModal(true)
+    }
+  }
+
+  return (
+    <Container onSubmit={handleSubmit}>
+      <div>
+        <label>Descrição</label>
+        <input placeholder="Ex: Alimentação" tabIndex="1" type="text" name="description" onChange={(e) => { setEntrieDescription(e.target.value) }} value={entrieDescription} />
+      </div>
+      <div>
+        <label>Valor</label>
+        <input placeholder="Ex: 1200" tabIndex="2" type="number" name="value" onChange={(e) => { setEntrieValue(e.target.value) }} value={entrieValue} />
+      </div>
+      <div>
+        <InputRadio onClick={() => { setEntrieType(true) }} type="radio" name="entrieType" value="entrada" tabIndex="3" defaultChecked />
+        <label>Entrada</label>
+        <InputRadio onClick={() => { setEntrieType(false) }} type="radio" name="entrieType" value="saida" tabIndex="4" />
+        <label>Saída</label>
+      </div>
+      <button tabIndex="5">Enviar</button>
+    </Container>
+  )
+}
--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,38 +1,14 @@
-import React, { useContext, useEffect, useState } from "react"
-import { EntriesContext } from "../../provider/EntriesProvider"
+import React, { useState } from "react"
 
 import { EntriesCard } from "../entriesCard"
 import { CardsStatistics } from "../CardsStatistics"
 import { ModalError } from "../ModalError"
+import { Form } from "../Form"
 
-import { ContainerMain, ContainerHeader, ContainerForm, ContainerEntries, HeaderEntries, InputRadio } from "./style"
+import { ContainerMain, ContainerHeader, ContainerEntries, HeaderEntries } from "./style"
 
 export function Home() {
-  const { AddEntrie, IdFromEntrie } = useContext(EntriesContext)
-
-  const [entrieType, setEntrieType] = useState(null)
-  const [entrieDescription, setEntrieDescription] = useState("")
-  const [entrieValue, setEntrieValue] = useState("")
   const [visibleModal, setVisibleModal] = useState(false)
-
-  const inputsRadios = document.querySelectorAll("input[name='entrieType']")
-
-  useEffect(() => {
-    const inputChecked = document.querySelector("input[name='entrieType']:checked").value
-    inputChecked === "entrada" ? setEntrieType(true) : setEntrieType(false)
-  }, [inputsRadios])
-
-  function handleSubmit(e) {
-    e.preventDefault()
-    if (entrieDescription !== '' && entrieValue !== 0) {
-      setEntrieDescription('')
-      setEntrieValue('')
-      AddEntrie(entrieDescription, entrieValue, entrieType)
-      IdFromEntrie()
-    } else {
-      setVisibleModal(true)
-    }
-  }
 
   return (
     <>
@@ -43,23 +19,7 @@ export function Home() {
 
       <ContainerMain>
         <CardsStatistics />
-        <ContainerForm onSubmit={handleSubmit}>
-          <div>
-            <label>Descrição</label>
-            <input placeholder="Ex: Alimentação" tabIndex="1" type="text" name="description" onChange={(e) => { setEntrieDescription(e.target.value) }} value={entrieDescription} />
-          </div>
-          <div>
-            <label>Valor</label>
-            <input placeholder="Ex: 1200" tabIndex="2" type="number" name="value" onChange={(e) => { setEntrieValue(e.target.value) }} value={entrieValue} />
-          </div>
-          <div>
-            <InputRadio onClick={() => { setEntrieType(true) }} type="radio" name="entrieType" value="entrada" tabIndex="3" defaultChecked />
-            <label>Entrada</label>
-            <InputRadio onClick={() => { setEntrieType(false) }} type="radio" name="entrieType" value="saida" tabIndex="4" />
-            <label>Saída</label>
-          </div>
-          <button tabIndex="5">Enviar</button>
-        </ContainerForm>
+         <Form setVisibleModal={setVisibleModal} />
 
         <ContainerEntries>
           <HeaderEntries>
